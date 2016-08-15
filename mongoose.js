6590fbb64b498c850f7de4a8daa730a81ab84a2d
OolgaGrig <skubalev@rambler.ru>
2016-08-15 20:02:51 +0300

    Add files via upload

--- mongoose.js
+++ mongoose.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/emyshop');
+mongoose.connect('mongodb://olgaGrig:SLdv16OI@ds161175.mlab.com:61175/mybase1');
 module.exports=mongoose;
